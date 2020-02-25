fe4da0a8cf779bc2e580db2a7b780cb7e7a5dbfc
Meirovingian <ageorge60420@gmail.com>
2020-02-25 16:12:39 +0100

    Start with cart content system

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class CartService {
 
+  items = [];
+
   constructor() { }
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  removeItemFromCart(item) {
+    this.items = this.items.filter(obj => obj !== item);
+    return this.items;
+  }
 
 }
---
+++ src/app/product-cart/product-cart.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-product-cart',
+  templateUrl: './product-cart.component.html',
+  styleUrls: ['./product-cart.component.css']
+})
+export class ProductCartComponent implements OnInit {
+
+  items = [];
+
+  constructor(
+    private cartService: CartService,
+  ) { }
+
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+  }
+
+  removeItemFromCart(item){
+    this.items = this.cartService.removeItemFromCart(item);
+  }
+
+}
